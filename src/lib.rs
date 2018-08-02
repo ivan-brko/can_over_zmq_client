a27a6d4995eef60f2df39f53357b617d5a33982b
Ivan Brko <ivan.brko@outlook.com>
2018-08-02 07:29:46 +0200

    Adding global variables

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,4 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+static mut SERVER_IP_ADDRESS : &str = "";
+static mut SERVER_PORT : u32 = 0;
+static mut TX : u32 = 0;
+static mut RX : u32 = 0;
