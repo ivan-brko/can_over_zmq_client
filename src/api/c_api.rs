a7cdddc6bfea3e045b791c038de471fa12edc04b
Ivan Brko <ivan.brko@outlook.com>
2018-08-17 09:34:14 +0200

    First complete draft of C-API completed

--- src/api/c_api.rs
+++ src/api/c_api.rs
@@ -13,8 +13,10 @@ use super::rust_api;
 pub extern "C" fn initialize_library(server_address: *const c_char, server_port_for_listener : u32, server_port_for_sender : u32, tx : u32, rx : u32, callback: extern "C" fn(*const libc::uint8_t, libc::size_t)) {
         let server_address = unsafe {CStr::from_ptr(server_address)};
         let server_address = String::from(server_address.to_str().unwrap());
-        rust_api::initialize_library(server_address, server_port_for_listener, server_port_for_sender, tx, rx, Box::new(|data: &[u8]| {
-           println!("adasdsa");
+        rust_api::initialize_library(server_address, server_port_for_listener, server_port_for_sender, tx, rx, Box::new(move |data: &[u8]| {
+           let data_as_ptr = data.as_ptr() as *const libc::uint8_t;
+           let length = data.len() as libc::size_t;
+           callback(data_as_ptr, length); //TODO: memory leak? who has to delete the data for each message?
         }));
 }
 
@@ -25,14 +27,3 @@ pub extern "C" fn send_can_message(array_pointer: *const libc::uint8_t, size: li
         let _ = rust_api::send_message(data_as_slice);
     }
 }
-
-// #[no_mangle]
-// pub extern fn rust_multiply(size: libc::size_t, array_pointer: *const libc::uint32_t) -> libc::uint32_t {
-//     internal_rust_multiply(unsafe { std::slice::from_raw_parts(array_pointer as *const i32, size as usize) }) as libc::uint32_t
-// }
-
-// fn internal_rust_multiply(array: &[i32]) -> i32
-// {
-//     assert!(!array.is_empty());
-//     array[0]
-// }
